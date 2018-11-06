a60c8aa6c5f8d052fdf25dabb1da0b1b8dd037ce
Razvan Uscatu <razvanusc@gmail.com>
2018-11-06 10:24:06 +0100

    finished-boats-controller

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -3,6 +3,10 @@ class BoatsController < ApplicationController
     @boats = Boat.all
   end
 
+  def show
+    @boat = Boat.find(params[:id])
+  end
+
   def new
     @boat = Boat.new
     @user = User.find(params[:user_id])
@@ -16,6 +20,23 @@ class BoatsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @boat = Boat.find(params[:id])
+    @user = User.find(params[:user_id])
+  end
+
+  def update
+    @boat = Boat.find(params[:id])
+    @boat.update(boat_params)
+    redirect_to user_boat_path(@boat)
+  end
+
+  def destroy
+    @boat = Boat.find(params[:id])
+    @boat.destroy
+    redirect_to user_boats_path
   end
 
   def boat_params
