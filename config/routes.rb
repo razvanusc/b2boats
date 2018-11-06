8703258f0a871518646a54e8e3f57aafb30c88ee
Razvan Uscatu <razvanusc@gmail.com>
2018-11-06 12:30:46 +0100

    Finished boat controller, changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :users, only: [:edit, :update] do
-    resources :boats
+    resources :boats, only: [:index]
   end
+  resources :boats
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
