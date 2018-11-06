c6552c4f2a3ae94b9c591dea64836e0857ac3ea0
Krisztina Kovari <krystonen@gmail.com>
2018-11-06 17:01:48 +0100

    Updated validations for boat, index, show, new pages

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,3 +1,10 @@
 class Boat < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence:true, uniqueness: true
+  validates :description, presence:true
+  validates :price_per_hour, presence:true
+  validates :hours, presence:true
+  validates :capacity, presence:true
+  validates :has_license, presence:true
 end
