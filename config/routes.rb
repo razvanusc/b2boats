3290ea92520cb31a06f4693728d55650f4d71146
Krisztina Kovari <krystonen@gmail.com>
2018-11-06 12:23:12 +0100

    Added show, edit, update actions to UsersController

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, only: [:edit, :update] do
+  resources :users, only: [:show, :edit, :update, :destroy] do
     resources :boats
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
