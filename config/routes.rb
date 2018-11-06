2ed81213cb160a922dd94f20d6afa31591e78e0a
Krisztina Kovari <krystonen@gmail.com>
2018-11-06 14:51:37 +0100

    Update routes.rb
    Routing conflict is fixed., We only have 1 nested page (boats#index)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, only: [:show, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit, :update, :destroy] do
+    resources :boats, only: [:index]
+  end
   resources :boats
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
