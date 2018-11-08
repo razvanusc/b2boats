d7ec3aafb415033584e6c31cbdbe48b56215fc13
Krisztina Kovari <krystonen@gmail.com>
2018-11-08 11:24:43 +0100

    Added cat and rating for boat, fixed edit/new

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_191026) do
+ActiveRecord::Schema.define(version: 2018_11_08_095206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2018_11_06_191026) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
+    t.integer "rating"
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
